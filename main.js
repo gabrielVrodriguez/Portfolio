858c9cb45d91da2a6f4d57cab9038d4befed6fe7
Gabriel <g.rodriguezsouza17@gmail.com>
2023-05-24 21:02:35 -0300

    commit last

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const btnMobile = document.querySelector("#btn-mobile");
 
-function toggleMenu() {
+function toggleMenu(event) {
+  if (event.type === "touchstart") event.preventDefault();
   const navBar = document.querySelector(".header-nav");
   navBar.classList.toggle("active");
 }
 
 btnMobile.addEventListener("click", toggleMenu);
+btnMobile.addEventListener("touchstart", toggleMenu);
